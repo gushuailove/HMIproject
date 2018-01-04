62f61456e6d0229237b0e6619c57a64daec324b3
gus <gushuai@123.com>
2018-01-04 22:05:41 +0800

    add Liyh demo code

---
+++ src/MCGSTouch.h
@@ -0,0 +1,44 @@
+#ifndef __MCGSTOUCH_H
+#define	__MCGSTOUCH_H
+
+#include "stm32f10x.h"
+#include "usart1.h"
+
+/**************串口初始化宏定义**************/
+#define		USATTBUF					Usart1Buf
+#define		USARTCONFIG				usart1_config
+#define		GETBUF						Get_Buf_Usart1_PT
+#define		SENDMESSAGE				usart1_send
+/********************************************/
+
+/******************Way类型******************/
+#define		WAY_1					1
+#define		WAY_2					2
+/********************************************/
+
+/******************Type类型******************/
+#define		DISPLAY_INTERFACE_1		1
+#define		DISPLAY_INTERFACE_2		2
+#define		DISPLAY_INTERFACE_3		3
+#define		DISPLAY_CONTINUED			4
+#define		DISPLAY_PAUSE					5
+#define		UPDATE_TIME						6
+#define		READ_KEY							7
+#define		EMPTY_KEY							8
+#define		SET_MODE_TIME					9
+/********************************************/
+
+/*********************Key********************/
+#define		KEY_START							0x0c
+#define		KEY_CONTINUED					0x04
+#define		KEY_PAUSE							0x06
+#define		KEY_CANCEL						0x08
+#define		KEY_ENTER							0x0a
+/********************************************/
+
+/*******************函数声明*****************/
+void MCGSTouch_Initial(void);
+void MCGSTouch_Send(uint8_t way,uint8_t type,uint8_t data1,uint8_t data2);
+uint8_t MCGSTouch_Receive(uint8_t type,uint8_t *key,uint8_t *way,uint8_t *time);
+/********************************************/
+#endif 
---
+++ src/main.c
@@ -0,0 +1,231 @@
+/******************** (C) COPYRIGHT 2012 WildFire Team **************************
+ * 文件名  ：main.c
+ * 描述    ：用3.5.0版本建的工程模板。         
+ * 实验平台：野火STM32开发板
+ * 库版本  ：ST3.5.0
+ *
+ * 作者    ：wildfire team 
+ * 论坛    ：http://www.amobbs.com/forum-1008-1.html
+ * 淘宝    ：http://firestm32.taobao.com
+**********************************************************************************/
+#include "stm32f10x.h"
+#include "timer_config.h"
+#include "MCGSTouch.h"
+
+uint8_t ii = 0;
+uint8_t key = 0;
+uint8_t way = 0;
+uint8_t time = 0;
+uint8_t jj = 0;
+uint8_t kk = 0;
+
+void delay(uint32_t ms)
+{
+  while(ms--);
+}
+
+
+/* 
+ * 函数名：main
+ * 描述  : 演示程序
+ * 输入  ：无
+ * 输出  : 无
+ */
+int main(void)
+{
+
+	Tim3_Config();
+	
+	//MCGSTouch_Initial初始化
+	MCGSTouch_Initial();
+	
+	while(1)
+	{
+		switch(ii)
+		{
+		/****************通道一：显示界面一*********************/
+			case 0:
+				MCGSTouch_Send(WAY_1,DISPLAY_INTERFACE_1,0,0);
+				ii++;
+				break;
+			case 1:
+				if(MCGSTouch_Receive(DISPLAY_INTERFACE_1,&key,&way,&time) == 1)
+				{
+					ii++;
+				}
+				break;
+		/*******************************************************/	
+		/**********通道一：询问界面一开始键是否被按下***********/					
+			case 2:
+				MCGSTouch_Send(WAY_1,READ_KEY,0,1);
+				ii++;
+				break;			
+			case 3:
+				jj = MCGSTouch_Receive(READ_KEY,&key,&way,&time);
+				if(jj == 3)
+				{
+					if(key==KEY_START)
+					{
+						ii++;
+						break;
+					}	
+					ii--;					
+				}
+				else if(jj == 2)
+				{
+					ii--;
+				}
+				break;
+		/*******************************************************/	
+		/***********通道一：设置界面一的模式和时间**************/	
+			case 4:
+				delay(0x1000000);
+				MCGSTouch_Send(WAY_1,SET_MODE_TIME,0,0);
+				ii++;
+				break;
+			case 5:
+				if(MCGSTouch_Receive(SET_MODE_TIME,&key,&way,&time) == 1)
+				{
+					ii++;					
+				}
+				break;
+		/*******************************************************/	
+		/********************通道一：清空按键*******************/	
+			case 6:
+				delay(0x1000000);
+				MCGSTouch_Send(WAY_1,EMPTY_KEY,0,1);
+				ii++;
+				break;
+			case 7:
+				if(MCGSTouch_Receive(EMPTY_KEY,&key,&way,&time) == 1)
+				{
+					ii++;
+				}
+				break;
+		/*******************************************************/	
+		/********************通道一：显示界面二*****************/	
+			case 8:
+				MCGSTouch_Send(WAY_1,DISPLAY_INTERFACE_2,1,1);
+				ii++;
+				break;
+			case 9:
+				if(MCGSTouch_Receive(DISPLAY_INTERFACE_2,&key,&way,&time) == 1)
+				{
+					ii++;
+				}
+				break;
+		/*******************************************************/	
+		/*******************通道一：设置剩余时间****************/					
+			case 10:
+				if(kk>30)
+				{
+					ii=ii+2;
+					kk=0;
+					break;
+				}
+				MCGSTouch_Send(WAY_1,UPDATE_TIME,1,kk);
+				delay(0x800000);
+				ii++;
+				break;
+			case 11:
+				if(MCGSTouch_Receive(UPDATE_TIME,&key,&way,&time) == 1)
+				{					
+					kk=kk+10;
+					ii--;
+				}
+				break;	
+		/*******************************************************/	
+		/************通道一：询问界面二某键是否被按下***********/					
+			case 12:
+				MCGSTouch_Send(WAY_1,READ_KEY,0,1);
+				ii++;
+				break;			
+			case 13:
+				jj = MCGSTouch_Receive(READ_KEY,&key,&way,&time);
+				if(jj == 3)
+				{
+					if(key ==KEY_CONTINUED)
+					{
+						ii =30;
+						break;
+					}
+					else if(key ==KEY_PAUSE)
+					{
+						ii =40;
+						break;
+					}
+					else if(key ==KEY_CANCEL)
+					{
+						ii++;
+						break;
+					}
+					ii--;
+				}
+				else if(jj == 2)
+				{
+					ii--;
+				}
+				break;
+		/*******************************************************/	
+		/*****************通道一：显示界面三********************/	
+			case 14:
+				MCGSTouch_Send(WAY_1,DISPLAY_INTERFACE_3,0,0);
+				ii++;
+				break;
+			case 15:
+				if(MCGSTouch_Receive(DISPLAY_INTERFACE_3,&key,&way,&time) == 1)
+				{
+					ii++;
+				}
+				break;
+		/*******************************************************/	
+		/************通道一：询问界面三某键是否被按下***********/	
+			case 16:
+				MCGSTouch_Send(WAY_1,READ_KEY,0,1);
+				ii++;
+				break;			
+			case 17:
+				jj = MCGSTouch_Receive(READ_KEY,&key,&way,&time);
+				if(jj == 3)
+				{
+					if(key ==KEY_ENTER)
+					{
+						ii=0;
+					}					
+				}
+				else if(jj == 2)
+				{
+					ii--;
+				}
+				break;
+		/*******************************************************/	
+		/*******************通道一：显示暂停键******************/	
+			case 30:
+				MCGSTouch_Send(WAY_1,DISPLAY_PAUSE,0,1);
+				ii++;
+				break;			
+			case 31:
+				if(MCGSTouch_Receive(DISPLAY_PAUSE,&key,&way,&time) == 1)
+				{
+					ii=12;
+				}
+				break;
+		/*******************************************************/	
+		/*******************通道一：显示继续键******************/	
+			case 40:
+				MCGSTouch_Send(WAY_1,DISPLAY_CONTINUED,0,1);
+				ii++;
+				break;			
+			case 41:
+				if(MCGSTouch_Receive(DISPLAY_CONTINUED,&key,&way,&time) == 1)
+				{
+					ii=12;
+				}
+				break;
+		/*******************************************************/	
+		}		
+	}
+}
+/******************* (C) COPYRIGHT 2012 WildFire Team *****END OF FILE************/
+
+
